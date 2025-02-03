2f535ed1c1ffb379586e36787fa6fd8590d81f3e
Roberto Fernández Iglesias <roberfi@gmail.com>
2025-02-03 19:12:56 +0100

    Add tailwindcss

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>Fun with fruits</h1>
+  <h1
+    class="text-5xl text-center my-5 py-5 mx-auto w-1/2 max-lg:w-9/10 bg-lime-200 text-lime-800 font-bold rounded-2xl border-2 border-lime-800"
+  >
+    Fun with fruits
+  </h1>
 </template>
